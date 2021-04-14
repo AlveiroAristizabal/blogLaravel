022ccb59afc7251a7b109c5ce756c10a413e59fa
jaabalv <alvinstrumentos@hotmail.com>
2021-04-13 22:37:41 -0500

    01dbRelacionadaBLOG
    post_tag

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+       User::create([
+          'name'=>'z',
+          'email' => 'z@z.com',
+          'password' => bcrypt('zzzzzzzz') 
+       ]);
+        User::factory(99)->create();
+       
+    }
+}
