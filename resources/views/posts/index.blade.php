4fd2cd39bebff95f930422786206640b2ce34d51
jaabalv <alvinstrumentos@hotmail.com>
2021-05-11 12:19:38 -0500

    17b
    17b

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,15 @@
             <div>
                 {{$post->imagen->url}}
                 {{Storage::url($post->imagen->url)}}
+                @foreach ($post->tags as $tag)
+                <a href="{{route('posts.tag', $tag)}}" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full">{{$tag->name}}</a>
+            @endforeach
+        </div>
+        <h1 class="text-4xl text-white leading-8 font-bold mt-2">
+            <a href="{{route('posts.show', $post)}}">
+                {{$post->name}}
+            </a>
+        </h1>
 
             </div>
             {{-- <article class="w-full h-80 bg-cover bg-center @if ($loop->first) md:col-span-2 @endif"                 
